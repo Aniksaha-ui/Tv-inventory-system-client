f853abcf1be43d1993c27d81eee913081eb3c977
Aniksaha-ui <sahaanik1045@gmail.com>
2022-05-03 01:15:22 +0600

    get single product details with authentication

--- src/Component/Inventory/Inventory.js
+++ src/Component/Inventory/Inventory.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import axios from "axios";
+import ProductDetails from "../ProductDetails/ProductDetails";
 
 const Inventory = () => {
+  const param = useParams();
+  const id = param.id;
+  const [user] = useAuthState(auth);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const getProduct = async () => {
+      const email = user.email;
+      const url = `https://guarded-spire-69476.herokuapp.com/product/${id}?email=${email}`;
+      try {
+        const { data } = await axios.get(url, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+
+        setProduct(data);
+      } catch {}
+    };
+
+    getProduct();
+  }, [user]);
+
   return (
-    <div>
-      <h3>This is for Inventory Management</h3>
-    </div>
+    <>
+      <ProductDetails />
+    </>
   );
 };
 
---
+++ src/Component/ProductDetails/ProductDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductDetails = () => {
+  return (
+    <div>
+      <h3>This is for Product details</h3>
+    </div>
+  );
+};
+
+export default ProductDetails;
