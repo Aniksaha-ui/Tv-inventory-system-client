1e63e792bc9bfde3c8f638c3e65d539403cb50a6
Aniksaha-ui <sahaanik1045@gmail.com>
2022-05-07 23:34:19 +0600

    invested product conditional rendering done

--- src/Component/Home/InvestedOnProduct/InvestedOnProduct.js
+++ src/Component/Home/InvestedOnProduct/InvestedOnProduct.js
@@ -2,15 +2,24 @@ import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import useProduct from "../../../Hooks/useProduct";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { useNavigate } from "react-router-dom";
 
 const InvestedOnProduct = () => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
   const [products, setProducts] = useProduct();
   return (
     <>
       <h3 className="mt-5 text-primary">Investement On Indiviual Product</h3>
 
       <div className="container d-flex align-items-center justify-content-center mt-4">
-        <table id="mytable" className="table table-dark">
+        <table className="table table-dark">
           <thead>
             <tr>
               <th>Name</th>
@@ -18,22 +27,45 @@ const InvestedOnProduct = () => {
               <th>img</th>
             </tr>
           </thead>
-          <tbody>
-            {products.slice(0, 6).map((product) => (
-              <tr key={product._id}>
-                <td>{product.name}</td>
-                <td>{product.quantity * product.price}</td>
-                <td>
-                  <img
-                    width="90px"
-                    className="img-fluid"
-                    src={product.img}
-                    alt=""
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          {user ? (
+            <>
+              <tbody>
+                {products.map((product) => (
+                  <tr key={product._id}>
+                    <td>{product.name}</td>
+                    <td>{product.quantity * product.price}</td>
+                    <td>
+                      <img
+                        width="90px"
+                        className="img-fluid"
+                        src={product.img}
+                        alt=""
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </>
+          ) : (
+            <>
+              <tbody>
+                {products.slice(0, 4).map((product) => (
+                  <tr key={product._id}>
+                    <td>{product.name}</td>
+                    <td>{product.quantity * product.price}</td>
+                    <td>
+                      <img
+                        width="90px"
+                        className="img-fluid"
+                        src={product.img}
+                        alt=""
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </>
+          )}
         </table>
       </div>
     </>
